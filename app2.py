202de3b832fee46e1605ae8823fe9a6d096ccd61
tiarafatihahk <109926716+tiarafatihahk@users.noreply.github.com>
2025-05-30 15:47:46 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -30,22 +30,12 @@ MODEL_FILENAME = 'best_random_forest_model.pkl'
 # Ganti daftar ini dengan fitur dan urutan yang sama persis seperti saat training model IDS Anda.
 # Saya menggunakan contoh fitur yang umum untuk kasus ini.
 FEATURE_ORDER = [
-    'network_packet_size', 
-    'login_attempts', 
-    'ip_reputation_score',
-    'failed_logins', 
-    'unusual_time_access', 
-    'browser_type_Chrome', 
-    'browser_type_Edge', 
-    'browser_type_Firefox', 
-    'browser_type_Safari',
-    'browser_type_Unknown', 
-    'protocol_type_ICMP', 
-    'protocol_type_TCP',
-    'protocol_type_UDP', 
-    'encryption_used_AES', 
-    'encryption_used_DES', 
-    'encryption_used_unencrypted'
+    'num__login_attempts','num__failed_logins','num__ip_reputation_score',
+    'num__network_packet_size','cat__unusual_time_access_Tidak',
+    'cat__unusual_time_access_Ya','cat__browser_type_Chrome',
+    'cat__browser_type_Firefox','cat__protocol_type_TCP',
+    'cat__protocol_type_UDP','cat__encryption_used_TLS'
+    'cat__encryption_used_Tidak Ada'
 ]
 # ----------------------------------------------------
 
