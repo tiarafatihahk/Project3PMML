5387c90a23995193f0e2e6a1c32c174d319330b2
tiarafatihahk <109926716+tiarafatihahk@users.noreply.github.com>
2025-06-03 05:12:54 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -12,7 +12,7 @@ import datetime
 # KONFIGURASI PUSAT
 # ======================================================================================
 
-MODEL_FILENAME = 'best_random_forest_model (2).pkl'
+MODEL_FILENAME = 'best_random_forest_model.pkl'
 
 # INI ADALAH DAFTAR 17 FITUR YANG PASTI DAN BENAR DARI NOTEBOOK ANDA
 FEATURE_ORDER = [
